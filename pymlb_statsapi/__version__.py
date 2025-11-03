2ac18ffaf4b968dd20c874f7527f1c1171da6866
Nikolaus Schuetz <nikolauspschuetz@gmail.com>
2025-11-02 20:02:03 -0500

    feat: add support for Python 3.10+
    - Update requires-python to >=3.10 (from >=3.13)
    - Update GitHub Actions workflow to test Python 3.10, 3.11, 3.12, 3.13
    - Update ruff target-version to py310
    - Update mypy python_version to 3.10
    - Add I001 to ignore list for auto-generated __version__.py

--- pymlb_statsapi/__version__.py
+++ pymlb_statsapi/__version__.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev12+gaadde69f3.d20251103"
-__version_tuple__ = version_tuple = (0, 1, "dev12", "gaadde69f3.d20251103")
+__version__ = version = "0.1.dev16+g2b138e71c.d20251103"
+__version_tuple__ = version_tuple = (0, 1, "dev16", "g2b138e71c.d20251103")
 
 __commit_id__ = commit_id = None
