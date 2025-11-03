60bdc44465dc4728cf0b0c8ad3565ce82f203ce7
Nikolaus Schuetz <nikolauspschuetz@gmail.com>
2025-11-02 20:34:34 -0500

    chore: rename workflow to ci-cd.yml and add twine to dev dependencies
    - Rename .github/workflows/test.yml to ci-cd.yml for clarity
    - Add twine>=6.2.0 to dev dependency group
    - Update build job to use --group dev for twine access
    - Update lockfile with new dependencies

--- pymlb_statsapi/__version__.py
+++ pymlb_statsapi/__version__.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.0'
-__version_tuple__ = version_tuple = (1, 0, 0)
+__version__ = version = '1.0.1.dev3+gb1cc1327a.d20251103'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev3', 'gb1cc1327a.d20251103')
 
 __commit_id__ = commit_id = None
